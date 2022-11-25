095633ae331d954cb46ab02f050bda300d2de18b
NeverMind109 <vladgladkih123@gmail.com>
2022-11-25 19:18:56 +0300

    Finished Navbar & Hero Components

--- hoo_bank/src/components/Hero.jsx
+++ hoo_bank/src/components/Hero.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import styles from '../style'
+import { discount, robot } from '../assets'
+import GetStarted from './GetStarted'
 
-const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+const Hero = () =>  (
+  <section className={`flex flex-col ${styles.paddingY} md:flex-row`} id="home">
+    <div className={`flex-1 ${styles.flexStart} flex-col px-6 xl:px-0 sm:px-16`}>
+      <div className="flex flex-row atems-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
+        <img className="w-[32px] h-[32px]" src={discount} alt="discount" />
+        <p className={`${styles.paragraph} ml-2`}>
+        <span className="text-white">20%</span> Discount For {" "}
+        <span className="text-white">1 Month</span> Account
+        </p>
+      </div>
+      <div className="flex flex-row justify-between items-center w-full">
+        <h1 className="flex-1 font-semibold text-[52px] leading-[75px] text-white ss:text-[72px] ss:leading-[100px]">
+        The Next<br className="hidden sm:block"/> {" "}
+        <span className="text-gradient">Generation</span> {" "}
+        
+        </h1>
+        <div className="hidden mr-0 md:mr-4 ss:flex">
+          <GetStarted />
+        </div>
+      </div>
+      <h2 className="font-semibold w-full text-[52px] leading-[75px] text-white mb-5 ss:text-[68px] ss:leading-[100px]">Payment Method.</h2>
+      <p className={`${styles.paragraph} max-w-[470px]`}>Our team of experts uses a methodology to identify the credit cards most likely to fit your needs. We examine annual percentage rates, annual fees.</p>
+    </div>
+
+    <div className={`relative flex flex-1 ${styles.flexCenter} my-10 md:my-0`}>
+      <img className="relative z-[5] w-[100%] h-[100%]" src={robot} alt="Billing" />
+
+        <div className="absolute top-0 z-[0] w-[40%] h-[35%] pink-gradient" />
+        <div className="absolute bottom-40 z-[1] w-[80%] h-[80%] rounded-full white-gradient" />
+        <div className="absolute bottom-20 right-20 z-[0] w-[50%] h-[50%] blue-gradient" />
+    </div>
+
+    <div className={`${styles.flexCenter} ss:hidden`}>
+      <GetStarted />
+    </div>
+  </section>
+)
 
 export default Hero
--- hoo_bank/src/components/Navbar.jsx
+++ hoo_bank/src/components/Navbar.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import { useState } from 'react';
+import { close, logo, menu } from '../assets';
+import { navLinks } from '../constants'
 
 const Navbar = () => {
-  return (
-    <div className="">
+  const [toggle, setToggle] = useState(false)
 
-    </div>
+  return (
+    <nav className="flex justify-between items-center w-full py-6 navbar">
+      <a href="#">
+        <img className="w-[124px] h-[32px]" src={logo} alt="Hoobank" />
+      </a>
+      <ul className="hidden justify-end items-center flex-1 list-none sm:flex">
+        {navLinks.map((nav, idx) => (
+          <li className={`font-normal text-[16px] text-white ${idx === navLinks.length - 1 ? 'mr-0' : 'mr-10'} cursor-pointer`} key={nav.id}>
+            <a className="" href={`#${nav.id}`}>{nav.title}</a>
+          </li>
+        ))}
+      </ul>
+      <div className="flex flex-1 justify-end items-center sm:hidden">
+        <img className="w-[28px] h-[28px] object-contain cursor-pointer" onClick={() => setToggle((prev) => !prev)} src={toggle ? close : menu} alt="Menu" />
+
+        <div className={`${toggle ? 'flex' : 'hidden'} absolute top-20 right-0 p-6 bg-black-gradient min-w-[140px] rounded-xl mx-4 my-2 sidebar`}>
+          <ul className="flex flex-col justify-end items-center flex-1 list-none">
+            {navLinks.map((nav, idx) => (
+              <li className={`font-normal text-[16px] text-white ${idx === navLinks.length - 1 ? 'mr-0' : 'mb-4'} cursor-pointer`} key={nav.id}>
+                <a className="" href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
   )
 }
 
